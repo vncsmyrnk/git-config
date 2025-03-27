2361ec14a90260bf91c5f48a345e739dbbf0734b
Vinicius Mayrink <vmayrink12@gmail.com>
2025-03-27 11:01:26 -0300

    chore(scripts): adds add me as reviewer script

---
+++ scripts/pr-add-me-as-reviewer.zsh
@@ -0,0 +1,23 @@
+#!/bin/zsh
+
+deps=(gh jq)
+command -v $deps >/dev/null || {
+  echo "Install all dependencies before using this command: ${deps[@]}"
+  exit 1
+}
+
+if [ -z "$1" ]; then
+  echo "usage: pr-add-me-as-reviewer <PR_NUMBER>"
+  exit 1
+fi
+
+# TODO: Fix util shift
+pr_number="$1"
+
+gh_user_name=$(gh api user | jq -r .login)
+if [ -z "$gh_user_name" ]; then
+  echo "not possible to retrieve GH user name via gh"
+  exit 1
+fi
+
+gh pr edit $pr_number --add-reviewer $gh_user_name
